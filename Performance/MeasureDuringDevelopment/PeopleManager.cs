a1ff08067aade7ecde9e5564802b8a5f52963fde
Rodolfo <rodph15@gmail.com>
2022-12-17 17:25:51 +0000

    init

---
+++ Performance/MeasureDuringDevelopment/PeopleManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasureDuringDevelopment
+{
+    internal static class PeopleManager
+    {
+        public static void AddPeople(ICollection<Person> peopleList)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                peopleList.Add(new Person
+                {
+                    Age = new Random().Next(1, 90)
+                });
+            }
+        }
+
+        public static void MeasureAges(ICollection<Person> peopleList)
+        {
+            var babies = peopleList.Where(x => x.Age < 2).Sum(x => x.Age);
+            var kids = peopleList.Where(x => x.Age > 2 && x.Age < 16).Sum(x => x.Age);
+            var adults = peopleList.Where(x => x.Age > 16 && x.Age < 60).Sum(x => x.Age);
+            var olds = peopleList.Where(x => x.Age > 60).Sum(x => x.Age);
+
+            Console.WriteLine($"Babies {babies}");
+            Console.WriteLine($"Kids {kids}");
+            Console.WriteLine($"Adults {adults}");
+            Console.WriteLine($"Olds {olds}");
+        }
+    }
+}
---
+++ Performance/MeasureDuringDevelopment/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MeasureDuringDevelopment;
+
+var peopleList = new List<Person>();
+
+PeopleManager.AddPeople(peopleList);
+PeopleManager.MeasureAges(peopleList);
